8351badd4ce87d462e519578d1c7063ee543947d
Praful Pawar <114899493+PRAFULPAWAR8888@users.noreply.github.com>
2026-01-12 09:36:39 +0530

    Time: 0 ms (100%), Space: 21.1 MB (5.81%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -4,5 +4,6 @@ class Solution:
 
         for i in range(len(nums)):
             if nums[i] in numsDict:
+                print("yes nums[i] in numsdicst")
                 return [numsDict[nums[i]], i]
             numsDict[target - nums[i]] = i
