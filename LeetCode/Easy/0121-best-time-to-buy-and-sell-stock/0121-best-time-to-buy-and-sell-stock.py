d2a3646e13a1c6f12dd96a3c691f4787357966d8
Praful Pawar <114899493+PRAFULPAWAR8888@users.noreply.github.com>
2026-01-13 10:37:34 +0530

    Time: 4 ms (99.61%), Space: 28.9 MB (5.69%) - LeetHub

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = prices[0] #price to buy 
-        max_profit = 0 #profit starts from 0
+        min_price = prices[0]   
+        max_profit = 0     
 
         for price in prices:
             if price < min_price:
-                min_price = price    #found cheaper day to  buy
-
+                min_price = price
             else:
-                profit = price - min_price #sell today
+                profit = price - min_price
                 if profit > max_profit:
                     max_profit = profit
         
         return max_profit
-        
