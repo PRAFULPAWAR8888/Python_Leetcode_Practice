9830d61c5c8ba87a3e058f2ec2b648db3637a8d9
Praful Pawar <114899493+PRAFULPAWAR8888@users.noreply.github.com>
2026-03-27 12:25:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/3856-trim-trailing-vowels/3856-trim-trailing-vowels.py
+++ LeetCode/Easy/3856-trim-trailing-vowels/3856-trim-trailing-vowels.py
@@ -1,14 +1,14 @@
 class Solution:
     def trimTrailingVowels(self, s: str) -> str:
-        vowels = "aeiou"
-        
-        i = len(s) - 1
-        
-        while i >= 0 and s[i] in vowels:
-            i -= 1
-        
-        return s[:i+1]
+       vowel = "aeiou"
 
+       i = len(s)-1
+
+       while i >= 0 and s[i] in vowels:
+        i -= 1
+
+        return s[:i+1]
+        
 
 
         
