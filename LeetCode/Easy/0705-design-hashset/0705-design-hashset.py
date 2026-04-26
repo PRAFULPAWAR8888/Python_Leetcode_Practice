37e7a46ed35f88223314c623a8e02f3c10fbf630
Praful Pawar <114899493+PRAFULPAWAR8888@users.noreply.github.com>
2026-04-26 16:41:21 +0530

    Time: 20 ms (87.7%), Space: 23.7 MB (99.73%) - LeetHub

--- LeetCode/Easy/0705-design-hashset/0705-design-hashset.py
+++ LeetCode/Easy/0705-design-hashset/0705-design-hashset.py
@@ -2,21 +2,21 @@ class MyHashSet:
     def __init__(self):
         self.NUM_BUCKETS = 1000
         self.buckets = [[] for _ in range(self.NUM_BUCKETS)]
-
-    def _hash(self, key: int) -> int:
+    
+    def _hash(self, key:int) -> int:
         return key % self.NUM_BUCKETS
-
-    def add(self, key: int) -> None:
+    
+    def add(self, key:int) -> None:
         bucket = self.buckets[self._hash(key)]
         if key not in bucket:
             bucket.append(key)
-
-    def remove(self, key: int) -> None:
+    
+    def remove(self, key:int) -> None:
         bucket = self.buckets[self._hash(key)]
         if key in bucket:
             bucket.remove(key)
-
-    def contains(self, key: int) -> bool:
+    
+    def contains(self, key:int) ->bool:
         return key in self.buckets[self._hash(key)]
         
 
