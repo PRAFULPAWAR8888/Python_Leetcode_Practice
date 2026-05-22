34e679feae96c7171fa715e199773f8bef72a410
Praful Pawar <114899493+PRAFULPAWAR8888@users.noreply.github.com>
2026-05-22 18:58:42 +0530

    Time: 4 ms (23.14%), Space: 19.3 MB (62.19%) - LeetHub

--- LeetCode/Easy/0389-find-the-difference/0389-find-the-difference.py
+++ LeetCode/Easy/0389-find-the-difference/0389-find-the-difference.py
@@ -1,19 +1,8 @@
 class Solution:
-    def my_ord(self, ch):
-        letters = "abcdefghijklmnopqrstuvwxyz"
-
-        for i in range(26):
-            if letters[i] == ch:
-                return i + 97
-
-    def my_chr(self, num):
-        letters = "abcdefghijklmnopqrstuvwxyz"
-        return letters[num - 97]
-
     def findTheDifference(self, s: str, t: str) -> str:
         result = 0
-
         for c in s + t:
-            result ^= self.my_ord(c)
-
-        return self.my_chr(result)
+            result ^= ord(c)
+        return chr(result)
+        
+         
