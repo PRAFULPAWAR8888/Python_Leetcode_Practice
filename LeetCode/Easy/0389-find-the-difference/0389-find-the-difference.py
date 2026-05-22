3aebfe586cfaa520b7c431722f6cbbb044bbc480
Praful Pawar <114899493+PRAFULPAWAR8888@users.noreply.github.com>
2026-05-22 18:50:31 +0530

    Time: 0 ms (100%), Space: 19.3 MB (24.41%) - LeetHub

--- LeetCode/Easy/0389-find-the-difference/0389-find-the-difference.py
+++ LeetCode/Easy/0389-find-the-difference/0389-find-the-difference.py
@@ -1,9 +1,8 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        lst = list(s)
-
-        for ch in t:
-            if ch in lst:
-                lst.remove(ch)
-            else:
-                return ch       
+        result = 0
+        for c in s + t:
+            result ^= ord(c)
+        return chr(result)
+        
+         
